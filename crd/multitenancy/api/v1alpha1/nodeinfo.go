5fd94a838de101468f51e8ec1dbac6a93a868bb1
Timothy J. Raymond <timraymond@users.noreply.github.com>
2025-06-24 14:03:44 -0400

    Add HomeAZ to NodeInfoStatus (#3738)
    In service of adding HomeAZ awareness to apiserver-vnet-integration,
    this adds HomeAZ to the NodeInfoStatus portion of the NodeInfo CRD.
    Similar to other elements of NodeInfoStatus, HomeAZ is an optional
    property of a node that can be communicated. If present, it strictly
    follows the AZXX format, where XX are two 0-9 integers.

--- crd/multitenancy/api/v1alpha1/nodeinfo.go
+++ crd/multitenancy/api/v1alpha1/nodeinfo.go
@@ -47,6 +47,10 @@ type NodeInfoStatus struct {
 type DeviceInfo struct {
 	DeviceType DeviceType `json:"deviceType,omitempty"`
 	MacAddress string     `json:"macAddress"`
+
+	// +kubebuilder:validation:optional
+	// +kubebuilder:validation:Pattern=`^AZ\d{2}$`
+	HomeAZ string `json:"homeAZ,omitempty"`
 }
 
 func init() {
