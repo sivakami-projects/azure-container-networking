b530c30e3df303fbf43727be4e70409034c54480
sivakami <sivakamis@microsoft.com>
2025-10-21 00:49:50 -0700

    Create clusters parallely

--- .pipelines/swiftv2-long-running/scripts/create_aks.sh
+++ .pipelines/swiftv2-long-running/scripts/create_aks.sh
@@ -17,17 +17,33 @@ az account set --subscription "$SUBSCRIPTION_ID"
 echo "==> Creating resource group: $RG"
 az group create -n "$RG" -l "$LOCATION" --output none
 
-# AKS clusters
-for CLUSTER in "aks-cluster-a" "aks-cluster-b"; do
+# Enable parallel cluster creation
+create_cluster() {
+  local CLUSTER=$1
   echo "==> Creating AKS cluster: $CLUSTER"
+
   az aks create -g "$RG" -n "$CLUSTER" -l "$LOCATION" \
     --network-plugin azure --node-count 1 \
     --node-vm-size "$VM_SKU_DEFAULT" \
     --enable-managed-identity --generate-ssh-keys \
-    --load-balancer-sku standard --yes
+    --load-balancer-sku standard --yes --only-show-errors
 
   echo "==> Adding high-NIC nodepool to $CLUSTER"
   az aks nodepool add -g "$RG" -n highnic \
     --cluster-name "$CLUSTER" --node-count 2 \
-    --node-vm-size "$VM_SKU_HIGHNIC" --mode User
-done
+    --node-vm-size "$VM_SKU_HIGHNIC" --mode User --only-show-errors
+
+  echo "Finished AKS cluster: $CLUSTER"
+}
+
+# Run both clusters in parallel
+create_cluster "aks-cluster-a" &
+pid_a=$!
+
+create_cluster "aks-cluster-b" &
+pid_b=$!
+
+# Wait for both to finish
+wait $pid_a $pid_b
+
+echo "AKS clusters created successfully!"
