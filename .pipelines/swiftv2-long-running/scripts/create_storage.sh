051afa8a060712736a0bafca9be96e5669ca82ee
sivakami <sivakamis@microsoft.com>
2025-10-22 17:05:06 -0700

    create storage account.

--- .pipelines/swiftv2-long-running/scripts/create_storage.sh
+++ .pipelines/swiftv2-long-running/scripts/create_storage.sh
@@ -9,28 +9,30 @@ RG=$3
 RAND=$(openssl rand -hex 4)
 SA1="sa1${RAND}"
 SA2="sa2${RAND}"
-API_VER="2025-06-01"
+
+# Set subscription context
+az account set --subscription "$SUBSCRIPTION_ID"
 
 # Create storage accounts
 for SA in "$SA1" "$SA2"; do
   echo "==> Creating storage account $SA"
-  az rest --method put \
-    --url "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RG/providers/Microsoft.Storage/storageAccounts/$SA?api-version=$API_VER" \
-    --body "{
-      \"location\": \"$LOCATION\",
-      \"sku\": { \"name\": \"Standard_LRS\" },
-      \"kind\": \"StorageV2\",
-      \"properties\": {
-        \"minimumTlsVersion\": \"TLS1_2\",
-        \"allowBlobPublicAccess\": false,
-        \"allowSharedKeyAccess\": false
-      }
-    }" \
+  az storage account create \
+    --name "$SA" \
+    --resource-group "$RG" \
+    --location "$LOCATION" \
+    --sku Standard_LRS \
+    --kind StorageV2 \
+    --allow-blob-public-access false \
+    --https-only true \
+    --min-tls-version TLS1_2 \
+    --query "name" -o tsv \
   && echo "Storage account $SA created successfully."
 done
 
 echo "All storage accounts created successfully."
+
+# Set pipeline output variables
 set +x
-	echo "##vso[task.setvariable variable=StorageAccount1;isOutput=true]$SA1"
-	echo "##vso[task.setvariable variable=StorageAccount2;isOutput=true]$SA2"
+echo "##vso[task.setvariable variable=StorageAccount1;isOutput=true]$SA1"
+echo "##vso[task.setvariable variable=StorageAccount2;isOutput=true]$SA2"
 set -x
