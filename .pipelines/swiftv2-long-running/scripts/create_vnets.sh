a6b33922da093e2b0bc20e02a190e1923fff75d0
sivakami <sivakamis@microsoft.com>
2025-10-23 12:18:54 -0700

    Link private endpoint dns to vnet a2 and vnet a3.

--- .pipelines/swiftv2-long-running/scripts/create_vnets.sh
+++ .pipelines/swiftv2-long-running/scripts/create_vnets.sh
@@ -19,10 +19,8 @@ A1_S2="10.10.2.0/24"
 A1_PE="10.10.100.0/24"
 
 A2_MAIN="10.11.1.0/24"
-A2_PE="10.11.100.0/24"
 
 A3_MAIN="10.12.1.0/24"
-A3_PE="10.12.100.0/24"
 
 B1_MAIN="10.20.1.0/24"
 
@@ -37,14 +35,10 @@ az network vnet subnet create -g "$RG" --vnet-name "$VNET_A1" -n pe --address-pr
 # A2
 az network vnet create -g "$RG" -n "$VNET_A2" --address-prefix 10.11.0.0/16 --subnet-name s1 --subnet-prefix "$A2_MAIN" -l "$LOCATION" --output none \
  && echo "Created $VNET_A2 with subnet s1"
-az network vnet subnet create -g "$RG" --vnet-name "$VNET_A2" -n pe --address-prefix "$A2_PE" --output none \
- && echo "Created $VNET_A2 with subnet pe"
 
 # A3
 az network vnet create -g "$RG" -n "$VNET_A3" --address-prefix 10.12.0.0/16 --subnet-name s1 --subnet-prefix "$A3_MAIN" -l "$LOCATION" --output none \
  && echo "Created $VNET_A3 with subnet s1"
-az network vnet subnet create -g "$RG" --vnet-name "$VNET_A3" -n pe --address-prefix "$A3_PE" --output none \
- && echo "Created $VNET_A3 with subnet pe"
 
 # B1
 az network vnet create -g "$RG" -n "$VNET_B1" --address-prefix 10.20.0.0/16 --subnet-name s1 --subnet-prefix "$B1_MAIN" -l "$LOCATION" --output none \
